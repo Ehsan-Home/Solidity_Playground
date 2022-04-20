eacd896bafa7963a20b56f067ca7eecd3e944c83
Remix Workspace <>
2022-04-19 17:32:17 -0700

    modifier

--- contracts/Mycontract.sol
+++ contracts/Mycontract.sol
@@ -3,20 +3,28 @@ pragma solidity ^0.8.13;
 contract MyContract {
     struct Car {
         string name;
-        uint16 price;
+        uint128 price;
     }
 
-    Car[] public cars;
+    uint numberOfCars = 0;
+    mapping (uint => Car) public cars;
+    address owner;
 
-    Car public myCar = Car("Opel", 1200);
-
-
-    function addCar(string memory _name, uint16 _price) public {
-        cars.push(Car(_name , _price));
+    modifier onlyOwner() {
+        require(owner == msg.sender , "Only owner can call this function");_;
     }
 
-    function getFirstCar() public view returns(Car memory){
-        return cars[0];
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addCar(string memory _name , uint128 _price) public onlyOwner {
+        cars[numberOfCars] = Car(_name , _price);
+        incrementNumberOfCars();
+    }
+
+    function incrementNumberOfCars() internal {
+        numberOfCars += 1;
     }
 
 }
